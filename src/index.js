c42d861e753fb2d2b9ffa49f7cb1fa9851e9dfb4
brandonocasey <brandonocasey@gmail.com>
2020-08-18 11:25:11 -0400

    refactor for tests

--- src/index.js
+++ src/index.js
@@ -1,70 +1,121 @@
 /* eslint-disable no-console */
-import requestStream from './request-stream.js';
-import MuxWorker from 'worker!../dist/mux-worker.worker.js';
-import EventTarget from './event-target.js';
+import XhrStreamer from './xhr-streamer.js';
+import {toUint8} from '@videojs/vhs-utils/dist/byte-helpers';
 import window from 'global/window';
+import EventTarget from './event-target';
 
-class XhrStreamer extends EventTarget {
-  constructor() {
+export default class SourceUpdater extends EventTarget {
+  constructor(el) {
     super();
-    this.worker_ = null;
-    this.handleMessage = this.handleMessage.bind(this);
+    this.el = el;
+    this.reset();
   }
 
-  createWorker_() {
-    if (this.worker_) {
+  reset() {
+    this.mse = new window.MediaSource();
+    if (this.url) {
+      window.URL.revokeObjectUrl(this.url);
+    }
+    this.url = window.URL.createObjectURL(this.mse);
+    this.el.src = this.url;
+    this.el.volume = 0.1;
+    this.queue = {};
+    this.buffers = {};
+    if (this.stream) {
+      this.stream.dispose();
+    }
+    this.stream = new XhrStreamer();
+  }
+
+  createBuffers(mimetypes) {
+    if (!this.mse.readyState === 'open') {
+      this.delayedCreateBuffers_ = this.createBuffers.bind(this, mimetypes);
+      this.mse.addEventListener('sourceopen', this.delayedCreateBuffers_);
+    }
+
+    this.mse.removeEventListener('sourceopen', this.delayedCreateBuffers_);
+    Object.keys(mimetypes).forEach((type) => {
+      console.log('creating sb ' + mimetypes[type]);
+      this.queue[type] = [];
+      this.buffers[type] = this.mse.addSourceBuffer(mimetypes[type]);
+      this.buffers[type].addEventListener('updateend', this.shiftQueue.bind(this, type));
+      this.mse.addEventListener('sourceopen', this.shiftQueue.bind(this, type));
+      this.shiftQueue(type);
+    });
+  }
+
+  queueAppend(type, data) {
+    this.queue[type].push(this.append.bind(this, type, data));
+    this.shiftQueue(type);
+  }
+
+  shiftQueue(type) {
+    if (!this.buffers[type] || this.buffers[type].updating || this.mse.readyState !== 'open' || !this.queue[type].length) {
       return;
     }
-
-    this.worker_ = new MuxWorker();
-    this.worker_.addEventListener('message', this.handleMessage);
-    this.worker_.postMessage({type: 'init', options: {allowPassthrough: false}});
+    this.queue[type].shift()();
   }
 
-  streamRequest(uri) {
-    console.log(`Streaming ${uri}`);
-    this.createWorker_();
-
-    const dataFn = (data) => {
-      this.worker_.postMessage({type: 'push', data: data.buffer}, [data.buffer]);
-    };
-    const doneFn = () => {
-      console.log('steam downloaded in ' + (window.performance.now() - this.streamStartTime) + 'ms');
-      this.abort_ = null;
-      this.worker_.postMessage({type: 'flush'});
-    };
-
-    this.streamStartTime = window.performance.now();
-    this.abort_ = requestStream(uri, dataFn, doneFn);
+  append(type, data) {
+    // console.log('appending ' + type + ' ' + nextdata.byteLength);
+    this.buffers[type].appendBuffer(toUint8(data));
   }
 
-  selectOutput(format) {
-    if (this.worker_) {
-      this.worker_.postMessage({type: 'output', output: format});
-    }
+  startStream(url, options) {
+    this.stream.streamRequest(url);
+    this.stream.on('data', (e) => {
+      const datatype = e.detail.datatype === 'muxed' ? 'video' : e.detail.datatype;
+
+      this.queueAppend(datatype, e.detail.data);
+    });
+
+    // TODO: api for selecting a format
+    this.stream.on('potential-formats', (e) => {
+      const supportedFormats = [];
+
+      for (let i = 0; i < e.detail.formats.length; i++) {
+        const format = e.detail.formats[i];
+
+        if (Object.keys(format.mimetypes).every((k) => window.MediaSource.isTypeSupported(format.mimetypes[k]))) {
+          supportedFormats.push(format);
+        }
+      }
+
+      this.trigger('supported-formats', {supportedFormats});
+
+    });
+
+    this.stream.on('unsupported', (e) => {
+      console.error(e.detail.reason);
+      this.reset();
+    });
+
+    const startTime = window.performance.now();
+
+    this.stream.on('done', (e) => {
+      const interval = window.setInterval(() => {
+        if (Object.keys(this.queue).some((q) => q.length !== 0)) {
+          return;
+        }
+
+        if (Object.keys(this.buffers).some((b) => b.updating)) {
+          return;
+        }
+
+        if (this.mse.readyState !== 'open') {
+          return;
+        }
+
+        console.log('calling end of stream after ' + (window.performance.now() - startTime) + 'ms');
+        this.mse.endOfStream();
+        window.clearInterval(interval);
+      }, 100);
+    });
   }
 
-  abort() {
-    if (this.abort_) {
-      this.abort_();
-    }
-
-    if (this.worker_) {
-      this.worker_.postMessage({type: 'abort'});
-    }
+  selectFormat(format) {
+    console.log('Selecting ', format);
+    this.createBuffers(format.mimetypes);
+    this.stream.selectOutput(format);
   }
-
-  handleMessage(e) {
-    const message = e.data;
-
-    this.trigger(message.type, message);
-  }
-
-  dispose() {
-    this.worker_.removeEventListener('message', this.handleMessage);
-    this.worker_.terminate();
-  }
-
 }
-
-export default XhrStreamer;
---
+++ src/xhr-streamer.js
@@ -0,0 +1,70 @@
+/* eslint-disable no-console */
+import requestStream from './request-stream.js';
+import MuxWorker from 'worker!../dist/mux-worker.worker.js';
+import EventTarget from './event-target.js';
+import window from 'global/window';
+
+class XhrStreamer extends EventTarget {
+  constructor() {
+    super();
+    this.worker_ = null;
+    this.handleMessage = this.handleMessage.bind(this);
+  }
+
+  createWorker_() {
+    if (this.worker_) {
+      return;
+    }
+
+    this.worker_ = new MuxWorker();
+    this.worker_.addEventListener('message', this.handleMessage);
+    this.worker_.postMessage({type: 'init', options: {allowPassthrough: false}});
+  }
+
+  streamRequest(uri) {
+    console.log(`Streaming ${uri}`);
+    this.createWorker_();
+
+    const dataFn = (data) => {
+      this.worker_.postMessage({type: 'push', data: data.buffer}, [data.buffer]);
+    };
+    const doneFn = () => {
+      console.log('steam downloaded in ' + (window.performance.now() - this.streamStartTime) + 'ms');
+      this.abort_ = null;
+      this.worker_.postMessage({type: 'flush'});
+    };
+
+    this.streamStartTime = window.performance.now();
+    this.abort_ = requestStream(uri, dataFn, doneFn);
+  }
+
+  selectOutput(format) {
+    if (this.worker_) {
+      this.worker_.postMessage({type: 'output', output: format});
+    }
+  }
+
+  abort() {
+    if (this.abort_) {
+      this.abort_();
+    }
+
+    if (this.worker_) {
+      this.worker_.postMessage({type: 'abort'});
+    }
+  }
+
+  handleMessage(e) {
+    const message = e.data;
+
+    this.trigger(message.type, message);
+  }
+
+  dispose() {
+    this.worker_.removeEventListener('message', this.handleMessage);
+    this.worker_.terminate();
+  }
+
+}
+
+export default XhrStreamer;
